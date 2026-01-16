aca4f64d09754bfa800193c3b92a92c91842c9d6
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-01-16 20:20:00 +0530

    feat: add FundooNotes login page with required fields

--- FundooNotes-uiux/src/app/pages/login/login.component.ts
+++ FundooNotes-uiux/src/app/pages/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
