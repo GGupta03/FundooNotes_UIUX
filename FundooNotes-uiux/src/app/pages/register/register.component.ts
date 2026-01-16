79de3739911102fe8d8243fa763111d4a0f1b764
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-01-16 20:37:37 +0530

    feat: build FundooNotes registration page UI

--- FundooNotes-uiux/src/app/pages/register/register.component.ts
+++ FundooNotes-uiux/src/app/pages/register/register.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
