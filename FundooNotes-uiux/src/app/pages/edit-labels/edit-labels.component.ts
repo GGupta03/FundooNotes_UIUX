449f578d52a5b311b4314eb4c9b1fafefc4df689
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-01-17 16:42:08 +0530

    feat: generate dashboard pages for notes, reminders, archive, trash and edit-labels

---
+++ FundooNotes-uiux/src/app/pages/edit-labels/edit-labels.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-labels',
+  imports: [],
+  templateUrl: './edit-labels.component.html',
+  styleUrl: './edit-labels.component.css'
+})
+export class EditLabelsComponent {
+
+}
